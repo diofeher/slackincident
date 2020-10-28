25417257fc36aa9acab85d1030b51127e5c88a2e
Diógenes Fernandes <dio.fernandes@messagebird.com>
2020-10-28 13:10:51 +0100

    refactor break glass

--- src/break-glass.js
+++ src/break-glass.js
@@ -3,20 +3,26 @@ const pagerduty = require('./integrations/pagerduty');
 const slack = require('./integrations/slack');
 
 
-const onBreakGlass = async (body) => {
-    const { text, channel_id, user_name, user_id } = body;
-    if(text.length < CONSTANTS.BREAK_GLASS_MINIMUM_LEN_DESCRIPTION) {
+const testIfIsChannelIncident = async(channelId, userId) => {
+    const { bot_id } = await slack.getProfileInfo();
+    const botUserInfo = await slack.getBotInfo(bot_id);
+    const { channel } = await slack.getChannelInfo(channelId);
+
+    if (botUserInfo.user_id != channel.creator) {
         var slackMessage = {
             icon_emoji: ':x:',
             attachments: [{
                 color: COLORS.RED,
-                text: `You need to specify a description when using /break-glass. Use like: /break-glass I want superpowers!`,
+                text: `This command can be used only on channels created by the bot. Break glass won't work here.`,
             }]
         };
-        slack.sendSlackMessageToChannel(user_id, slackMessage);
-        return;
+        slack.sendSlackMessageToChannel(userId, slackMessage);
+        return true;
     }
+    return false;
+}
 
+const testTotalActiveIncidents = async (userId) => {
     const totalActiveEvents = await pagerduty.getTotalActiveIncidents();
     if (totalActiveEvents == 0) {
         var slackMessage = {
@@ -27,13 +33,17 @@ const onBreakGlass = async (body) => {
             }]
         };
         slack.sendSlackMessageToChannel(user_id, slackMessage);
-        return;
+        return true;
     }
+    return false;
+}
 
+const testTimeout = async(channelId, username) => {
     const currentTime = new Date();
     const pagerDutyDetails = await pagerduty.getIncidentBySlackChannel(channel_id);
     const incidentCreatedTime = new Date(pagerDutyDetails.created_at);
     const delta = (currentTime - incidentCreatedTime);
+
 
     if (delta / 1000 > CONSTANTS.BREAK_GLASS_OFFTIME) {
         var slackMessage = {
@@ -43,25 +53,40 @@ const onBreakGlass = async (body) => {
                 color: COLORS.RED,
             }],
         };
-        slack.sendSlackMessageToChannel(channel_id, slackMessage);
-        return;
+        slack.sendSlackMessageToChannel(channelId, slackMessage);
+        return true;
     }
+    return false;
+}
 
-    const { bot_id } = await slack.getProfileInfo();
-    const botUserInfo = await slack.getBotInfo(bot_id);
-    const { channel } = await slack.getChannelInfo(channel_id);
-
-    if (botUserInfo.user_id != channel.creator) {
+const testMinimumLength = async(userId, text) => {
+    if(text.length < CONSTANTS.BREAK_GLASS_MINIMUM_LEN_DESCRIPTION) {
         var slackMessage = {
             icon_emoji: ':x:',
             attachments: [{
                 color: COLORS.RED,
-                text: `This command can be used only on channels created by the bot. Break glass won't work here.`,
+                text: `You need to specify a good description (minimum ${CONSTANTS.BREAK_GLASS_MINIMUM_LEN_DESCRIPTION} characters) when using /break-glass. Use like: /break-glass I want superpowers!`,
             }]
         };
-        slack.sendSlackMessageToChannel(user_id, slackMessage);
-        return;
+        slack.sendSlackMessageToChannel(userId, slackMessage);
+        return true;
     }
+    return false;
+}
+
+const onBreakGlass = async (body) => {
+    const { text, channel_id, user_name, user_id } = body;
+
+    const errors = Promise.all([
+        await testMinimumLength(user_id, text),
+        await testTotalActiveIncidents(user_id),
+        await testIfIsChannelIncident(channel_id, user_id),
+        await testTimeout(channel_id, user_name),
+    ])
+
+    if(errors.some(Boolean)) {
+        return;
+    };
 
     var slackMessage = {
         icon_emoji: ':fire_engine:',
